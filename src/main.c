c9068e4dd58d6a460b699545f4bde458d6683892
lulu731 <lulu731lulu@hotmail.fr>
2025-06-26 16:48:19 +0200

    fix: drive RESET_ADC GPIO

--- src/main.c
+++ src/main.c
@@ -1,25 +1,54 @@
-/* SPI Master Half Duplex EEPROM example.
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-
 #include "wifi_connect.h"
 #include "spi_master.h"
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <inttypes.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/gpio.h"
+
+#define GPIO_HEATER    18
+#define GPIO_RESET_ADC 40
+#define GPIO_PIN_SEL   ((1ULL<<GPIO_HEATER) | (1ULL<<GPIO_RESET_ADC))
+/*
+ * Let's say, GPIO_OUTPUT_IO_0=18, GPIO_OUTPUT_IO_1=19
+ * In binary representation,
+ * 1ULL<<GPIO_OUTPUT_IO_0 is equal to 0000000000000000000001000000000000000000 and
+ * 1ULL<<GPIO_OUTPUT_IO_1 is equal to 0000000000000000000010000000000000000000
+ * GPIO_OUTPUT_PIN_SEL                0000000000000000000011000000000000000000
+ * */
+
 
 void app_main(void)
 {
-    connect_to_wifi();
+//    connect_to_wifi();
+
+    //zero-initialize the config structure.
+    gpio_config_t io_conf = {};
+    //disable interrupt
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    //set as output mode
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    //bit mask of the pins that you want to set,e.g.GPIO18/19
+    io_conf.pin_bit_mask = GPIO_PIN_SEL;
+    //disable pull-down mode
+    io_conf.pull_down_en = 0;
+    //disable pull-up mode
+    io_conf.pull_up_en = 0;
+    //configure GPIO with the given settings
+    gpio_config(&io_conf);
+
+    gpio_set_level(GPIO_RESET_ADC, 1);
+    vTaskDelay(10000 / portTICK_PERIOD_MS);
     test_spi();
 
+    int cnt = 0;
     while (1) {
-        // Add your main loop handling code here.
-        vTaskDelay(100);
+        printf("cnt: %d\n", cnt++);
+        vTaskDelay(10000 / portTICK_PERIOD_MS);
+        gpio_set_level(GPIO_HEATER, cnt % 2);
     }
 }
