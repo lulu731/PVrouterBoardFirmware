1a8987b75b6ec2a41ead68230d4fe32a853a944d
lulu731 <lulu731lulu@hotmail.fr>
2025-08-16 23:39:43 +0200

    test: merge tests concerning calibration params loading

--- lib/calibration/calibration_helper.h
+++ lib/calibration/calibration_helper.h
@@ -1,6 +1,24 @@
 #ifndef CALIBRATION_HELPER_H
 #define CALIBRATION_HELPER_H
 
+#ifdef TEST
+    #include <stdint.h>
+    /**
+    * @brief Load a parameter from to_param_array with the value of the given key.
+    *
+    * @param key The key to search for in the non-volatile storage.
+    * @param key_array An array of key strings available for this parameter. The
+    *                  length of this array must be the same as to_param_array.
+    * @param value The value to store in the param.
+    * @param to_param_array An array of pointers to uint16_t params. If the key is found,
+    *                       the value will be stored in the corresponding element of this array.
+    * @param array_keys_count The length of the key_array or to_param_array.
+    */
+    void load_param(const char*    key,   const char* key_array[],
+                    const uint16_t value, uint16_t*   to_param_array[],
+                    const int      array_keys_count);
+#endif
+
 int load_calibration_params(void);
 
 #endif // CALIBRATION_HELPER_H
