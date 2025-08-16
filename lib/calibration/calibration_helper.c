b6999e31ed5e349a7b9d10e2030a4baa4d461f8b
lulu731 <lulu731lulu@hotmail.fr>
2025-08-16 23:14:18 +0200

    feat: implement load_calibration_params function in calibration_helper files

---
+++ lib/calibration/calibration_helper.c
@@ -0,0 +1,49 @@
+#include "calibration_helper.h"
+
+#include "nvs_driver.h"
+#include "nvs_functions.h"
+
+#include "calibration_params.h"
+
+#include <stddef.h>
+#include <string.h>
+
+/**
+ * @brief Load a parameter from to_param_array with the value of the given key.
+ *
+ * @param key The key to search for in the non-volatile storage.
+ * @param key_array An array of key strings available for this parameter. The
+ *                  length of this array must be the same as to_param_array.
+ * @param value The value to store in the param.
+ * @param to_param_array An array of pointers to uint16_t params. If the key is found,
+ *                       the value will be stored in the corresponding element of this array.
+ * @param array_keys_count The length of the key_array or to_param_array.
+ */
+void load_param(const char*    key,   const char* key_array[],
+                const uint16_t value, uint16_t*   to_param_array[],
+                const int      array_keys_count)
+{
+    for (int i = 0; i < array_keys_count; i++)
+        if (strcmp(key, key_array[i]) == 0)
+        {
+            *to_param_array[i] = value;
+            return;
+        }
+}
+
+int load_calibration_params(void)
+{
+    int data_count = 0;
+
+    nvs_data_t nvs_data = get_first_nvs_data();
+
+    while (nvs_data.key != NULL)
+    {
+        load_param(nvs_data.key, string_keys_array, nvs_data.value, keys_array, keys_count);
+        data_count++;
+
+        nvs_data = get_next_nvs_data();
+    }
+
+    return data_count;
+}
---
+++ lib/calibration/calibration_helper.h
@@ -0,0 +1,6 @@
+#ifndef CALIBRATION_HELPER_H
+#define CALIBRATION_HELPER_H
+
+int load_calibration_params(void);
+
+#endif // CALIBRATION_HELPER_H
