4dc0be9bd2971f4e9dd2d99e81de3cf1a5662b52
lulu731 <lulu731lulu@hotmail.fr>
2025-09-01 15:37:21 +0200

    feat: implement server_start with errors

--- lib/server/server.c
+++ lib/server/server.c
@@ -6,13 +6,18 @@
 
 static httpd_handle_t web_server = NULL;
 static httpd_config_t config;
+
+static esp_err_t index_handler(httpd_req_t *req)
+{
+    return ESP_OK;
+}
+
 static httpd_uri_t index_uri = {
     .uri       = "/",
     .method    = HTTP_GET,
-    .handler   = NULL,
+    .handler   = index_handler,
     .user_ctx  = NULL
 };
-
 
 void server_create(void)
 {
@@ -21,8 +26,15 @@ void server_create(void)
 
 server_err_t server_start(void)
 {
-    httpd_start(web_server, &config);
-    httpd_register_uri_handler(web_server, &index_uri);
+    esp_err_t err = httpd_start(web_server, &config);
+
+    if (err == ESP_OK)
+        err = httpd_register_uri_handler(web_server, &index_uri);
+
+    if (err != ESP_OK)
+        return SERVER_ERROR;
+
+    return SERVER_OK;
 }
 
 /*static void server_stop(void)
--- lib/server/server.h
+++ lib/server/server.h
@@ -5,7 +5,8 @@
 
 typedef uint8_t server_err_t;
 
-#define SERVER_OK 0
+#define SERVER_OK    0
+#define SERVER_ERROR 1
 
 void server_create(void);
 server_err_t server_start(void);
