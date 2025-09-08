7a7466cd348f239eb0bc245236f1314f79877a7a
lulu731 <lulu731lulu@hotmail.fr>
2025-09-08 22:04:05 +0200

    feat: update main.c to test use of websocket

--- lib/server/server.c
+++ lib/server/server.c
@@ -1,8 +1,11 @@
 #include "server.h"
 
 #include "esp_http_server.h"
+#include "esp_log.h"
 
+#include <stdio.h>
 #include <stddef.h>
+#include <string.h>
 
 #ifndef TEST
 #define STATIC static
@@ -10,11 +13,62 @@
 #define STATIC
 #endif
 
-static httpd_handle_t web_server = NULL;
-static httpd_config_t config;
+#define TAG "server.c: "
+
+static httpd_handle_t web_server;
+static httpd_config_t config = HTTPD_DEFAULT_CONFIG();
 
 static esp_err_t index_handler(httpd_req_t *req)
 {
+    ESP_LOGI(TAG, "Reading %s", "/index.html");
+    FILE* pfile = fopen("/index.html", "r");
+
+    if (pfile == NULL)
+    {
+        httpd_resp_send_err(req, 404, "File Not Found");
+        ESP_LOGE(TAG, "fopen fail.");
+        return ESP_FAIL;
+    }
+    else
+    {
+		char line[128];
+		while (fgets(line, sizeof(line), pfile) != NULL) {
+			/*size_t linelen = strlen(line);
+			//remove EOL (CR or LF)
+			for (int i=linelen;i>0;i--) {
+				if (line[i-1] == 0x0a) {
+					line[i-1] = 0;
+				} else if (line[i-1] == 0x0d) {
+					line[i-1] = 0;
+				} else {
+					break;
+				}
+			}*/
+			//ESP_LOGD(TAG, "line=[%s]", line);
+			//if (strlen(line) == 0) continue;
+			esp_err_t ret = httpd_resp_sendstr_chunk(req, line);//, HTTPD_RESP_USE_STRLEN);
+			if (ret != ESP_OK) {
+				ESP_LOGE(TAG, "httpd_resp_sendstr_chunk fail %d", ret);
+			}
+		}
+		fclose(pfile);
+
+        int i = 0;
+        while (1) {
+            char data[11];
+            sprintf(data, "%d", i++);
+
+            httpd_ws_frame_t frame =
+            {
+                .fragmented = 0,
+                .type = HTTPD_WS_TYPE_TEXT,
+                .payload = (uint8_t*)data,
+                .len = strlen(data)
+            };
+            httpd_ws_send_frame(req, &frame);
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+        }
+	}
     return ESP_OK;
 }
 
@@ -28,7 +82,7 @@ STATIC httpd_uri_t index_uri = {
 
 void server_create(void)
 {
-    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+    web_server = NULL;
 }
 
 server_err_t server_start(void)
--- src/main.c
+++ src/main.c
@@ -1,8 +1,4 @@
 #include "wifi_connect.h"
-#include "spi_master.h"
-#include "spi_functions.h"
-#include "nvs_functions.h"
-#include "nvs_params.h"
 
 #include <stdio.h>
 #include <string.h>
@@ -12,6 +8,8 @@
 #include "freertos/task.h"
 #include "freertos/queue.h"
 #include "driver/gpio.h"
+#include "server.h"
+
 
 #define GPIO_HEATER    18
 #define GPIO_RESET_ADC 40
@@ -19,17 +17,19 @@
 
 void app_main(void)
 {
-    esp_err_t nvs_error = nvs_init();
+    /*esp_err_t nvs_error = nvs_init();
     assert(nvs_error == ESP_OK);
     nvs_handle_t nvs_handle;// = 0;
     nvs_open_file("meter_config", &nvs_handle);
     load_nvs_params(nvs_handle);
-    nvs_close(nvs_handle);
+    nvs_close(nvs_handle);*/
 
     connect_to_wifi();
 
+    server_create();
+    server_start();
     //zero-initialize the config structure.
-    gpio_config_t io_conf = {};
+    /*gpio_config_t io_conf = {};
     //disable interrupt
     io_conf.intr_type = GPIO_INTR_DISABLE;
     //set as output mode
@@ -49,7 +49,7 @@ void app_main(void)
     vTaskDelay(10 / portTICK_PERIOD_MS);
     gpio_set_level(GPIO_RESET_ADC, 1);
     vTaskDelay(1000 / portTICK_PERIOD_MS);
-    calibrate_adc();
+    calibrate_adc();*/
 
     while (1) {
         vTaskDelay(1000 / portTICK_PERIOD_MS);
