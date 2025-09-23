6732d8949c57b5305057351af017de0832d401a8
lulu731 <lulu731lulu@hotmail.fr>
2025-09-23 22:51:43 +0200

    test(server): add test to send a string to ws

--- test/server/test_server.c
+++ test/server/test_server.c
@@ -8,6 +8,9 @@
 
 #include "esp_err.h"
 
+#include <string.h>
+#include <stdlib.h>
+
 static httpd_handle_t web_server = NULL;
 static int server_handle = 100;
 
@@ -95,20 +98,35 @@ void test_send_to_all_clients_should_return_number_of_clients(void)
     httpd_get_client_list_ReturnThruPtr_fds(&fds);
     httpd_get_client_list_ReturnArrayThruPtr_client_fds(client_fds, fds);
 
-    //httpd_queue_work_ExpectAndReturn(web_server, NULL, &client_fds[0], ESP_OK);
-    //httpd_queue_work_IgnoreArg_work();
-    //httpd_ws_send_frame_async_ExpectAndReturn(web_server, client_fds[0], NULL, ESP_OK);
-    //httpd_ws_send_frame_async_IgnoreArg_frame();
+    struct work_fn_arg* arg = malloc(sizeof(struct work_fn_arg));
+    httpd_ws_frame_t* frame = malloc(sizeof(httpd_ws_frame_t));
+    frame->fragmented = 0;
+    frame->type = HTTPD_WS_TYPE_TEXT;
+    frame->payload = (uint8_t*)message;
+    frame->len = strlen(message);
+    arg->frame = frame;
+    arg->sock_fd = client_fds[0];
 
-    //httpd_queue_work_ExpectAndReturn(web_server, NULL, &client_fds[1], ESP_OK);
-    //httpd_queue_work_IgnoreArg_work();
-    //httpd_ws_send_frame_async_ExpectAndReturn(web_server, client_fds[1], NULL, ESP_OK);
-    //httpd_ws_send_frame_async_IgnoreArg_frame();
+    httpd_queue_work_ExpectWithArrayAndReturn(web_server, NULL, arg, 1, ESP_OK);
+    httpd_queue_work_IgnoreArg_work();
 
-    httpd_queue_work_ExpectAnyArgsAndReturn(ESP_OK);
-    httpd_queue_work_ExpectAnyArgsAndReturn(ESP_OK);
+    struct work_fn_arg* arg2 = malloc(sizeof(struct work_fn_arg));
+    httpd_ws_frame_t* frame2 = malloc(sizeof(httpd_ws_frame_t));
+    frame2->fragmented = 0;
+    frame2->type = HTTPD_WS_TYPE_TEXT;
+    frame2->payload = (uint8_t*)message;
+    frame2->len = strlen(message);
+    arg2->frame = frame2;
+    arg2->sock_fd = client_fds[1];
+    httpd_queue_work_ExpectWithArrayAndReturn(web_server, NULL, arg2, 1, ESP_OK);
+    httpd_queue_work_IgnoreArg_work();
 
     size_t result = server_send_to_all_clients(message);
+
+    free(frame);
+    free(arg);
+    free(frame2);
+    free(arg2);
 
     TEST_ASSERT_EQUAL_UINT8(fds, result);
 }
