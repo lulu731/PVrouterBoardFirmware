bd5768f8946a79de940caf0a092793bd48e4b2c1
lulu731 <lulu731lulu@hotmail.fr>
2025-10-09 23:04:30 +0200

    implement index uri

--- lib/server/server.c
+++ lib/server/server.c
@@ -15,61 +15,6 @@
 static httpd_handle_t web_server;
 static httpd_config_t config = HTTPD_DEFAULT_CONFIG();
 
-static esp_err_t load_html(const char* file, httpd_req_t *req)
-{
-    ESP_LOGI(TAG, "Reading %s", file);
-    FILE* pfile = fopen(file, "r");
-    if (pfile == NULL)
-    {
-        httpd_resp_send_err(req, 404, "File Not Found");
-        ESP_LOGE(TAG, "fopen fail.");
-        return ESP_FAIL;
-    }
-    else
-    {
-        char line[128];
-        while (fgets(line, sizeof(line), pfile) != NULL) {
-            esp_err_t ret = httpd_resp_sendstr_chunk(req, line);
-            if (ret != ESP_OK) {
-                ESP_LOGE(TAG, "httpd_resp_sendstr_chunk fail %d", ret);
-            }
-        }
-        fclose(pfile);
-    }
-    return httpd_resp_sendstr_chunk(req, NULL);
-}
-
-static esp_err_t save_req_session_context(httpd_req_t *req)
-{
-    req->sess_ctx = malloc(sizeof(int));
-    if (req->sess_ctx == NULL)
-    {
-        ESP_LOGE(TAG, "malloc req->sess_ctx fail");
-        return ESP_FAIL;
-    }
-    *(int*)(req->sess_ctx) = httpd_req_to_sockfd(req);
-    return ESP_OK;
-}
-
-static esp_err_t handler_first_call(httpd_req_t *req, const char* file)
-{
-    esp_err_t err = ESP_OK;
-    if (!req->sess_ctx)
-    {
-        err = save_req_session_context(req);
-        if (err == ESP_OK)
-            err = load_html(file, req);
-    }
-    return err;
-}
-
-static esp_err_t index_handler(httpd_req_t *req)
-{
-    esp_err_t err = ESP_OK;
-    err = handler_first_call(req, "/littlefs/index.html");
-    return err;
-}
-
 static httpd_uri_t index_uri = {
     .uri       = "/",
     .method    = HTTP_GET,
@@ -104,7 +49,6 @@ static httpd_uri_t ws_uri = {
     .user_ctx  = NULL,
     .is_websocket = true
 };
-
 
 void server_create(void)
 {
---
+++ lib/server/uri_handlers.h
@@ -0,0 +1,9 @@
+#ifndef URI_HANDLERS_H
+#define URI_HANDLERS_H
+
+#include "esp_http_server.h"
+#include "esp_err.h"
+
+esp_err_t index_handler(httpd_req_t *req);
+
+#endif // URI_HANDLERS_H
