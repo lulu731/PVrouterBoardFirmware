1a171bdc07d71f0be2d82c8485ab91ad8b9b5156
lulu731 <lulu731lulu@hotmail.fr>
2026-03-13 22:11:53 +0100

    test(test-support-esp-idf): add esp_timer mock for unit testing

---
+++ test/support/esp-idf/src/esp_timer.c
@@ -0,0 +1,22 @@
+#include "esp_timer.h"
+
+esp_err_t esp_timer_create(const esp_timer_create_args_t* create_args,
+                           esp_timer_handle_t* out_handle)
+{
+    return ESP_OK;
+};
+
+esp_err_t esp_timer_start_periodic(esp_timer_handle_t timer, uint64_t period)
+{
+    return ESP_OK;
+}
+
+esp_err_t esp_timer_delete(esp_timer_handle_t timer)
+{
+    return ESP_OK;
+};
+
+esp_err_t esp_timer_stop(esp_timer_handle_t timer)
+{
+    return ESP_OK;
+};
